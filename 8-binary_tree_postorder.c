7562db83826fa00cfdd675d5db9640c9199b60a5
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-07 12:27:59 +0200

    Updated header, created source file for task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_postorder - A function that goes thtough a binary tree
+ *							using post-order traversal
+ *
+ * @tree: A pointer to the root of the tree
+ * @func: A pointer to a function to call for each node
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	printf("%d\n", tree->n);
+}
