fbcc4e1e2d56f83483e7e474fc8468d7da1c01e8
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-08 14:12:53 +0200

    Tried something

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,8 +16,6 @@ size_t binary_tree_h(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL || tree->right == NULL)
-		return (1);
 
 	left_h = binary_tree_h(tree->left);
 	right_h = binary_tree_h(tree->right);
